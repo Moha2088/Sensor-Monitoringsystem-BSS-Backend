84e3d6a482d1c9e5998a4e4e084d40d682f01236
Mohamed Shil <maxamed-14@outlook.dk>
2024-11-23 12:27:15 +0100

    add inmemorydb

--- BSS_Backend_Opgave.Tests/IntegrationTests/Factory/CustomWebApplicationFactory.cs
+++ BSS_Backend_Opgave.Tests/IntegrationTests/Factory/CustomWebApplicationFactory.cs
@@ -1,9 +1,11 @@
 ﻿using BSS_Backend_Opgave.API;
+using BSS_Backend_Opgave.Repositories.Data;
 using BSS_Backend_Opgave.Repositories.Models.Dtos;
 using BSS_Backend_Opgave.Services.Service.Interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -15,10 +17,6 @@ namespace BSS_Backend_Opgave.Tests.IntegrationTests.Factory
 {
     public class CustomWebApplicationFactory : WebApplicationFactory<IApiMarker>
     {
-        public CustomWebApplicationFactory()
-        {
-            
-        }
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
@@ -26,7 +24,11 @@ namespace BSS_Backend_Opgave.Tests.IntegrationTests.Factory
 
             builder.ConfigureTestServices(service =>
             {
-                
+                service.AddDbContext<BSS_Backend_OpgaveAPIContext>(opt =>
+                {
+                    opt.UseSqlServer()
+                    .UseInMemoryDatabase("TestDBIntegration");
+                });
             });
         }
 
