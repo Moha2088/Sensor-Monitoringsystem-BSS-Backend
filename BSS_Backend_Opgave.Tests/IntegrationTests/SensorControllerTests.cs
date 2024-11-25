26d72235dc28b6ae0b472d00e20978c957ac36f8
Mohamed Shil <maxamed-14@outlook.dk>
2024-11-25 13:20:55 +0100

    add SensorControllerTest

---
+++ BSS_Backend_Opgave.Tests/IntegrationTests/SensorControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using BSS_Backend_Opgave.Models;
+using BSS_Backend_Opgave.Repositories.Data;
+using BSS_Backend_Opgave.Repositories.Models.Dtos.SensorDtos;
+using BSS_Backend_Opgave.Tests.IntegrationTests.Factory;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace BSS_Backend_Opgave.Tests.IntegrationTests
+{
+    public class SensorControllerTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private HttpClient _client;
+        private readonly CustomWebApplicationFactory _factory;
+
+        public SensorControllerTests(CustomWebApplicationFactory factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task CreateSensor_ShouldReturn201Created_WhenCreated()
+        {
+            var sensor = new SensorCreateDto
+            {
+                Name = "TestSensor",
+                Location = "TestLocation"
+            };
+
+            var result = await _client.PostAsJsonAsync("api/sensors", sensor);
+
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+    }
+}
