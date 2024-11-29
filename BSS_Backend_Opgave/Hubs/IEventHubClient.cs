0b7d12173564dd6cd5c3da1b1a787b14bef95057
Mohamed <maxamed-14@outlook.dk>
2024-11-29 11:41:28 +0100

    delete redundant method from hub interface

--- BSS_Backend_Opgave/Hubs/IEventHubClient.cs
+++ BSS_Backend_Opgave/Hubs/IEventHubClient.cs
@@ -10,11 +10,4 @@ public interface IEventHubClient
     /// <param name="message"></param>
     /// <returns></returns>
     public Task ReceiveMessage(string message);
-
-    /// <summary>
-    /// Sends information about the sensor which state has changed
-    /// </summary>
-    /// <param name="id">The dto that contains information about the event</param>
-    /// <returns></returns>
-    public Task OnStateChanged(string dtoJson);
 }
